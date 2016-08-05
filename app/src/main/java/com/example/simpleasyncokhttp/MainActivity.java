886fbdde6272aa441caa6e525f6ef07b17187ea0
ghalibse <ghalibse@gmail.com>
2016-08-05 14:49:08 -0400

    Second Commit

--- app/src/main/java/com/example/simpleasyncokhttp/MainActivity.java
+++ app/src/main/java/com/example/simpleasyncokhttp/MainActivity.java
@@ -2,29 +2,34 @@ package com.example.simpleasyncokhttp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView mlistView;
+    private Button mlistView;
+    private final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mlistView = (ListView) findViewById(R.id.lst_view);
+        mlistView = (Button) findViewById(R.id.btn1);
 
-        doMagic();
     }
 
-    protected String doMagic(String... strings) {
+    protected void doMagic(View view) {
 
         String url = "http://www.mocky.io/v2/57a01bec0f0000c10d0f650f";
 
@@ -33,13 +38,19 @@ public class MainActivity extends AppCompatActivity {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
-        try {
-            Response response = client.newCall(request).execute();
-            String JSONstring = response.body().string();
-            return JSONstring;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                    Log.d(TAG, "onResponse: " + response.body().string());
+
+                }
+            });
     }
 }
